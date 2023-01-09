c62dbdafe318e2692a2947a8454a34ff93cc842d
ItSerje <66359825+ItSerje@users.noreply.github.com>
2023-01-09 16:00:57 +0600

    configure metro.config.ts to bundle firebase

---
+++ metro.config.js
@@ -0,0 +1,9 @@
+require('ts-node/register');
+module.exports = require('./metro.config.ts');
+
+const { getDefaultConfig } = require('@expo/metro-config');
+
+const defaultConfig = getDefaultConfig(__dirname);
+defaultConfig.resolver.assetExts.push('cjs');
+
+module.exports = defaultConfig;
